8757acd4cad8478ac8b126364a8926e450a20831
Seoyeon Kim <kseoy0607@gmail.com>
2024-12-24 17:07:00 +0900

    241224 카드 뭉치

---
+++ programmers/lv1/bundle-of-card.js
@@ -0,0 +1,12 @@
+function solution(cards1, cards2, goal) {
+  let idx1 = 0;
+  let idx2 = 0;
+
+  for (let word of goal) {
+    if (cards1[idx1] === word) idx1++;
+    else if (cards2[idx2] === word) idx2++;
+    else return 'No';
+  }
+
+  return 'Yes';
+}
